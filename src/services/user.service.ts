0d0c269c9c88d7621da7b913b1474676a2c33f86
Faysal2123 <foysal2005n1@gmail.com>
2026-09-06 00:33:12 +0600

    feat: add environment variable validation

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,15 +1,19 @@
+import { env } from "@/env";
 import { cookies } from "next/headers";
+
+const AUTH_URL = env.AUTH_URL;
 
 export const userService = {
   getSession: async function () {
     try {
       const cookieStore = await cookies();
-      const res = await fetch("http://localhost:5000/api/auth/get-session", {
+      const res = await fetch(`${AUTH_URL}/get-session`, {
         headers: {
           Cookie: cookieStore.toString(),
         },
         cache: "no-store",
       });
+
       const session = await res.json();
       if (session === null) {
         return { data: null, error: { message: "No session found" } };
