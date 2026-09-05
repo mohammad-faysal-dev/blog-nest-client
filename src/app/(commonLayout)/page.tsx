3d76dbd03d73eb4421b6d76427c6a50988f50096
Faysal2123 <foysal2005n1@gmail.com>
2026-09-05 20:41:56 +0600

    update common layout page

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,10 +1,20 @@
-import { authClient } from "@/lib/auth-client";
+import { cookies } from "next/headers";
 import React from "react";
 
-const page = async () => {
-  const session = await authClient.getSession();
-  console.log(session);
+const Page = async () => {
+  const cookieStore = await cookies();
+
+  console.log("cookieStore:", cookieStore);
+
+  const res = await fetch("http://localhost:3000/api/auth/get-session", {
+    headers: {
+      Cookie: cookieStore.toString(),
+    },
+    cache: "no-store",
+  });
+
+  console.log(res.json());
   return <div>this is the common layout page</div>;
 };
 
-export default page;
+export default Page;
