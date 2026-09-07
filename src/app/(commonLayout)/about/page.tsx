0b0debba0e65dab803c2233d7b219cee6cac53a6
Faysal2123 <foysal2005n1@gmail.com>
2026-09-08 02:13:03 +0600

    feat: add server action to fetch blogs

--- src/app/(commonLayout)/about/page.tsx
+++ src/app/(commonLayout)/about/page.tsx
@@ -1,6 +1,19 @@
-import { Button } from "@/components/ui/button"
+"use client";
+import { getBlogs } from "@/actions/blog.action";
+import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
 
-export default function Home() {
+export default function AboutPage() {
+  const [data, setData] = useState();
+  const [error, setError] = useState<{message:string}|null>(null);
+  console.log("data", data);
+  useEffect(() => {
+    (async () => {
+      const { data, error } = await getBlogs();
+      setData(data);
+      setError(error);
+    })();
+  }, []);
   return (
     <div>
       <Button variant="outline">Button</Button>
